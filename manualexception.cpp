5e0350fb305c59855de81714e9466acd4cacdc16
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-10 18:49:19 +0700

    try

--- manualexception.cpp
+++ manualexception.cpp
@@ -1,3 +1,12 @@
 #include <iostream> 
 using namespace std;
 
+int main()
+{
+    try{
+        cout << "selamat belajar di produ TI UMY " << endl;
+        throw 0,5; //melemparkan sebuat integer maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+
+}
