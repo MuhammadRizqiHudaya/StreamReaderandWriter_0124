3b5c0de2a792d26d56c30bbf4285853bf4afeb1a
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-10 17:40:50 +0700

    jika ada perulangan

--- iofile.cpp
+++ iofile.cpp
@@ -24,5 +24,17 @@ int main (){
     outfile.close();
     //membuka file dalam mode membaca
     ifstream infile;
-    
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+    cout << endl << ">= membaca file" << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+
 }
