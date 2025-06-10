99f41dbd9f41d6db16ab3093ba344e378ffa920a
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-10 18:49:35 +0700

    catch int a

--- manualexception.cpp
+++ manualexception.cpp
@@ -8,5 +8,9 @@ int main()
         throw 0,5; //melemparkan sebuat integer maka
         cout << "pernyataan tidak akan dieksekusi" << endl;
     }
-
+    catch (int a){
+        //blok ini akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    
 }
