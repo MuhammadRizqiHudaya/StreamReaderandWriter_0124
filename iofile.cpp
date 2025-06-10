a98f5543f02d31bd9fc127a529650b6078ec0cc1
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-10 17:39:45 +0700

    unlimited loop untuk menulis

--- iofile.cpp
+++ iofile.cpp
@@ -10,5 +10,15 @@ int main (){
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
     cout << ">= menulis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true){
+        cout << "-";
+        //mendapatkan setiap karakter dalam bentuk satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
     
 }
